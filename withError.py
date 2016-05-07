a6dca852cd7e09f4618ec579b08202d03d34ad89
camclark <cameron.clark1@my.jcu.edu.au>
2016-05-07 23:56:35 +1000

    Added end display

--- withError.py
+++ withError.py
@@ -1,3 +1,10 @@
+def greeting():
+    # appears on startup, asks for users name. Response saved as userName.
+    print("Please enter your name:")
+    userName = input()
+    print("Hello " + userName + " Welcome to the Tropical Airlines .")
+    return userName
+
 def checkHandle(prompt,codeArray,wordArray):
     switch = False
     while switch == False:
@@ -17,6 +24,27 @@ def checkHandle(prompt,codeArray,wordArray):
                         print("Error input not recognised. \nPlease select what you would like by entering the corosponding letter in brackets")
     return instanceInput
 
+def getName(userName):
+    # asks user if ticket for them or other. If other asks for that persons name. Includes userproofing.
+    switch = False
+    while switch == False:
+        print("Is the ticket for (Y)ou or (S)omeone else?")
+        ticketFor = input()
+        ticketFor = ticketFor.lower()
+        if  ticketFor == "y":
+            passengerName = userName
+            switch = True
+        elif ticketFor == "s":
+            print("Please enter the name of the person traveling")
+            passengerName = input()
+            if passengerName == " " or passengerName == "":
+                print("ERROR: Please input a name")
+            else:
+                switch = True
+        else:
+                print("ERROR: Input not recognised. Please input either (Y) for the ticket to be for yourself, or (S) for the ticket to be for someone else.")
+    return passengerName
+
 def getAge():
     # asks user for the passenger's age. Ensures age is within reasonable range.
     switch = False
@@ -34,16 +62,87 @@ def getAge():
             print("Please ensure you entered the correct age of the passenger")
     return passengerAge
 
-def orderTicket(userName):
+def formatCurrency(cost):
+    # Formats cost
+    cost = "${:,.2f}".format(cost)
+    return cost
 
-    age = getAge()
+
+def costCalculation(tripDestination, flightType, seatType, seatClass, passengerAge):
+    # Calculates cost of trip from expected values
+    if tripDestination == "Cairns" and flightType == "one-way":
+        tripValue = 250
+    elif tripDestination == "Cairns" and flightType == "return":
+        tripValue = 400
+    elif tripDestination == "Sydney" and flightType == "one-way":
+        tripValue = 420
+    elif tripDestination == "Sydney" and flightType == "return":
+        tripValue = 575
+    elif tripDestination == "Perth" and flightType == "one-way":
+        tripValue = 510
+    else:
+        tripValue = 700
+
+    if seatClass == "business":
+        classValue = 275
+    elif seatClass == "economy":
+        classValue = 25
+    else:
+        classValue = 0
+
+    if seatType == "window":
+        typeValue = 75
+    elif seatType == "aisle":
+        typeValue = 50
+    else:
+        typeValue = -25
+
+    if passengerAge <= 2:
+        ageDiscount = 0
+    elif passengerAge <= 16 and passengerAge >= 3:
+        ageDiscount = 0.5
+    else:
+        ageDiscount = 1
+
+    cost = (tripValue + classValue + typeValue) * ageDiscount
+    cost = "${:,.2f}".format(cost)
+    return cost
+
+
+
+def acceptPurchase():
+    # presents user with a y/n to purchase ticket.
+    # should make it to return cost 0 if not accepted
+    acceptSwitch = False
+    while acceptSwitch == False:
+        print("Would you like to purchase this ticket: (Y)es or (N)o ")
+        acceptPurchase = input()
+        acceptPurchase = acceptPurchase.lower()
+        if acceptPurchase == "y":
+            print("Your ticket has been purchased. Thank you for flying with Tropical Airlines. You have been returned to the main menu.")
+            acceptSwitch = True
+        elif acceptPurchase == "n":
+            print("Your ticket has not been purchased. You have been returned to the main menu.")
+            acceptSwitch = True
+        else:
+            print("ERROR: Input not recognised. Please enter Y for yes or N for No.")
+    return acceptPurchase
+
+
+
+
+
+def orderTicket(userName):
+    passengerName = getName(userName)
+
+    passengerAge = getAge()
 
     codeArray = ["c", "s", "p"]
     wordArray = ["Cairns","Sydney","Perth"]
     prompt = "Please select the destination for your return trip. Base fare prices are listed below: \n(C)airns – $400 \n(S)ydney – $575 \n(P)erth - $700"
 
     tripDestination= checkHandle(prompt,codeArray,wordArray)
-    print("Testing full word print; " + tripDestination)
+
 
 
     codeArray = ["o", "r"]
@@ -52,7 +151,7 @@ def orderTicket(userName):
 
 
     flightType = checkHandle(prompt,codeArray,wordArray)
-    print("Testing full word print; " + flightType)
+
 
 
     codeArray = ["b", "e","f"]
@@ -60,17 +159,24 @@ def orderTicket(userName):
     prompt = "Please choose the type of fare. Fees are displayed below and are in addition to the basic fare. \nPlease note choosing Frugal fare means you will not be offered a seat choice. \n(B)usiness - $275 \n(E)conomy - $25 \n(F)rugal - $0"
 
     seatType = checkHandle(prompt, codeArray, wordArray)
-    print("Testing full word print; " + seatType)
+
 
     if seatType == "frugal":
-        seatClass = middle
+        seatClass = "middle"
     else:
         codeArray = ["w", "a","m"]
         wordArray = ["window", "aisle", "middle"]
         prompt = "Please choose the seat type.  Choosing the middle seat will deduct 25 from the total fare. \n(W)indow  $75 \n(A)isle  $50 \n(M)iddle -$25"
 
         seatClass = checkHandle(prompt, codeArray, wordArray)
-    print("Testing full word print; " + seatClass)
+
+
+    cost = costCalculation(tripDestination,flightType,seatType,seatClass,passengerAge)
+
+    print(" Ticket for: " + passengerName  +"\nPassenger age: " + str(passengerAge) + "\nTrip destination: " + tripDestination + "(" + flightType + ") " + "\nSeat class: " + seatClass + "\nSeat type: " + seatType + "\nTotal cost: " + cost)
+
+    return cost
+
 
 
 def main():
@@ -111,3 +217,4 @@ def main():
             print("ERROR: Input not recognised. Please enter the letter in brackets to complete the intended action.")
 
 main()
+
